33325b59b6694526d02aab2940bc7808533373f2
MICHAEL WILLIAMS <MICHAELWILLIAMSPERSONAL@GMAIL.COM>
2024-03-24 21:06:02 -0700

    first commit

---
+++ src/main.rs
@@ -0,0 +1,98 @@
+mod prompt;
+
+use std::io;
+use std::path::Path;
+use colored::*;
+
+// personal modules
+use prompt::*;
+
+use image;
+
+fn supports_format(extension: &str) -> bool {
+    match extension {
+        "jpg" => true,
+        "png" => true,
+        "ico" => true,
+        "bmp" => true,
+        "tiff" => true,
+        "avif" => true,
+        "webp" => true,
+        _ => false
+    }
+}
+
+fn main() -> io::Result<()> {
+    println!("--- Image Converter ---");
+
+    println!("{} Enter an filepath (including filename & extension):", ">".green());
+    let init_filepath_raw = prompt(|response| {
+        let path = Path::new(&response);
+        
+        if path.exists() && path.is_file() {
+            if let Some(extension) = path.extension().to_owned() {
+                let extension_str = extension.to_string_lossy();
+
+                if supports_format(&extension_str.into_owned().as_str()) {
+                    return Some(response);
+                } else {
+                    println!("{}", "File type not supported.".red().bold());
+                }
+            }
+        }
+        println!("{}", "Please enter a valid filepath.".red().bold());
+        return None
+    });
+
+    let init_path = Path::new(&init_filepath_raw);
+
+    println!("{} Enter directory for exported file:", ">".green());
+    let out_dir_raw = prompt(|response| {
+        let path = Path::new(&response);
+
+        if path.exists() {
+            return Some(response);
+        }
+        println!("{}", "Please enter a valid filepath.".red().bold());
+        None
+    });
+
+    let out_dir = Path::new(&out_dir_raw);
+
+    println!("{} Enter name (with extension) of exported file:", ">".green());
+    let out_file_raw = prompt(|response| {
+        if response.len() > 0 {
+            let path = Path::new(&response);
+
+            if let Some(extension) = path.extension().to_owned() {
+                let extension_str = extension.to_string_lossy();
+            
+                if supports_format(&extension_str.into_owned().as_str()) {
+                    return Some(response);
+                } else {
+                    println!("{}", "File type not supported.".red().bold());
+                    return None;
+                }
+            } else {
+                println!("{}", "Please enter a valid filename".red().bold());
+                return None;
+            }
+        }
+        println!("{}", "Name must not be blank.".red().bold());
+        None
+    });
+
+    let out_file = Path::new(&out_file_raw);
+
+    let img = image::io::Reader::open(init_path)?
+        .decode().unwrap();
+
+    println!("{}", "Converting...".yellow());
+    match img.save(out_dir.join(out_file).as_path()) {
+        Ok(_) => (),
+        Err(e) => println!("{}", e)
+    };
+    println!("{} {}", "Converted! output in:".green(), out_dir.to_string_lossy().cyan().bold());
+
+    Ok(())
+}
---
+++ src/prompt.rs
@@ -0,0 +1,24 @@
+use std::{io, str::FromStr};
+
+pub fn io_get_strip() -> Option<String> {
+    let mut buf = String::new();
+
+    match io::stdin().read_line(&mut buf) {
+        Ok(_) => Some(buf.trim().to_string()),
+        Err(_) => None
+    }
+}
+
+pub fn prompt<T, F>(validate_fn: F) -> T 
+where
+    T: FromStr,
+    F: Fn(String) -> Option<T>
+{
+    loop {
+        if let Some(raw_response) = io_get_strip() {
+            if let Some(validated_response) = validate_fn(raw_response) {
+                return validated_response;
+            }
+        }
+    }
+}
